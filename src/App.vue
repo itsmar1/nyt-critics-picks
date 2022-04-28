2d8986b2220784d632f48195a65e72c3619d5a86
Marouane Ahal <d144013152@gmail.com>
2022-04-28 00:12:33 +0000

    create the filter section

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <the-header></the-header>
-  <the-filters></the-filters>
-  <h1 class="text-5xl text-center font-bold text-green-400 tracking-widest">Hello world</h1>
+  <the-filters @fetch-movies="handleFilters" ></the-filters>
 </template>
 
 <script>
@@ -9,12 +8,51 @@
 
 import TheHeader from './components/layout/TheHeader.vue'
 import TheFilters from './components/layout/TheFilters.vue'
+import { API_KEY } from './config/constants';
 
 export default {
   name: 'App',
   components: {
     TheHeader,
     TheFilters
+  },
+  data() {
+    return {
+      moviesData: [],
+      errors: null,
+      loading: false,
+      offsetNum: 0,
+      movieOrder: "by-opening-date",
+    }
+  },
+  methods: {
+    handleFilters(movieOrder, search) {
+      this.fetchMovies(this.offsetNum, movieOrder, search);
+    },
+    async fetchMovies(offsetNum = 0, movieOrder = "by-opening-date", query = "") {
+        this.error = null;
+        this.loading = true;
+        try {
+            const response = await fetch(`https://api.nytimes.com/svc/movies/v2/reviews/picks.json?api-key=${API_KEY}&offset=${offsetNum}&order=${movieOrder}&query=${query}`);
+            if (!response.ok) {
+                const error = new Error("Failed to fetch Data");
+                error.statusCode = 404;
+                throw error;
+            }
+            else {
+                const data = await response.json();
+                this.moviesData = data.results;
+                this.loading = false;
+                console.log(data.results);
+            }
+        } catch (error) {
+            console.log(error);
+            this.error = "Something went wrong - try again later!";
+        }
+    }
+  },
+  async mounted() {
+       await this.fetchMovies();
   }
 }
 </script>
--- src/components/layout/TheFilters.vue
+++ src/components/layout/TheFilters.vue
@@ -1,13 +1,58 @@
 <template>
-  <h1>TheFilters</h1>
+    <div class="container mx-auto">
+        <div>
+            <div class="flex justify-around">
+                <div class="flex items-center p-4 space-x-2">
+                    <span class="fa fa-search"></span>
+                    <input type="search" name="search" id="search" placeholder="Search" 
+                        class="bg-gray-100 p-2 rounded-md"
+                        @change="handleFilters"
+                        v-model.trim="search">
+                </div>
+                <div class="flex items-center p-4 space-x-2">
+                    <label for="sortBy">Sort By: </label>
+                    <select name="sortBy" id="sortBy" v-model="movieOrder" @change="handleFilters" class="rounded-md bg-gray-100 p-2 cursor-pointer">
+                        <option value="by-opening-date">Opening Date</option>
+                        <option value="by-publication-date">Publication Date</option>
+                        <option value="by-title">Title</option>
+                    </select>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
+
+
+
 <script>
+
+
 export default {
+    emits: ['fetch-movies'],
+    data() {
+        return {
+            search: "",
+            movieOrder: "by-opening-date"
+        }
+    },
+    methods: {
+        handleFilters(event) {
+            const name = event.target.name;
+            const value = event.target.value;
+            if (name === 'search') {
+                this.search = value;
+            }
+            if (name === 'sortBy') {
+                this.movieOrder = value;
+            }
+            console.log('Name: ', name);
+            console.log('Value: ', value);
+            this.$emit('fetch-movies', this.movieOrder, this.search);
+        }
+    }
 
 }
 </script>
 
-<style>
 
-</style>
